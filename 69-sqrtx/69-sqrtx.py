911d4365d933ad83606c8e3c67fe78f6f3e9e0b5
Anushka Kapoor <63553019+Anushka1902@users.noreply.github.com>
2022-08-15 16:32:10 +0530

    Time: 69 ms (41.76%), Space: 13.7 MB (96.36%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,6 +1,25 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        #binary-Search
+        s=0
+        e=x
+        while(s<=e):
+            mid=s+(e-s)//2
+            if mid*mid==x:
+                return mid
+            elif mid*mid<x:
+                ans=mid
+                s=mid+1
+            else:
+                e=mid-1
+        return ans
+        #linear Search
+        #SC-O(1)
+        #TC-O(rootx)
+        '''
         y=0
         while(y*y<=x):
             y+=1
         return y-1
+        '''
+    
