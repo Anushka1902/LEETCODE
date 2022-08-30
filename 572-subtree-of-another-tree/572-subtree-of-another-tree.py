1174afa6197aec53a71f7f0eabbd28afb0d82f20
Anushka Kapoor <63553019+Anushka1902@users.noreply.github.com>
2022-08-30 09:29:23 +0530

    Time: 126 ms (89.61%), Space: 15.2 MB (36.06%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -12,10 +12,9 @@ class Solution:
             if not t1 and t2 or not t2 and t1 or t1.val != t2.val: 
                 return False
             return check(t1.left,t2.left) and check(t1.right,t2.right)
+        if subRoot==None:
+            return True
+        if root==None:
+            return False
+        return check(root,subRoot) or self.isSubtree(root.left,subRoot)or self.isSubtree(root.right,subRoot)
         
-        def dfs(node):
-            if not node: return False
-            if check(node,subRoot):
-                return True
-            return dfs(node.left) or dfs(node.right)
-        return dfs(root)
