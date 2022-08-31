8f5b34818b8d219aa3c1b1e0b83fb6ed041c0946
Anushka Kapoor <63553019+Anushka1902@users.noreply.github.com>
2022-08-31 11:36:25 +0530

    Time: 3333 ms (15.07%), Space: 29.3 MB (91.27%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
@@ -6,6 +6,7 @@ class Solution:
             return gcd(b%a,a)
         def lcm(a,b):
             return (a*b)//gcd(a,b)
+        '''
         stack=[nums[0]]
         for i in nums[1:]:
             y=i
@@ -14,4 +15,11 @@ class Solution:
                 y=lcm(x,y)
             stack.append(y)
         return stack
+        '''
+        ans = []
+        for i in nums:
+            while ans and gcd(ans[-1],i)>1:
+                i=lcm(ans.pop(),i)
+            ans.append(i)
+        return ans
             
