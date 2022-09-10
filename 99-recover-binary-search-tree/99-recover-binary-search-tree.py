1f3b43b8b71c1f08bbbc6de4e65b7d01e7d2b5e8
Anushka Kapoor <63553019+Anushka1902@users.noreply.github.com>
2022-09-10 09:32:51 +0530

    Time: 173 ms (6.66%), Space: 14.2 MB (64.54%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -6,29 +6,16 @@
 #         self.right = right
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        res=[]
-        def inorder(rootpa):
-            if not rootpa:
+        temp = []
+    
+        def dfs(node):
+            if not node:
                 return
-            inorder(rootpa.left)
-            res.append(rootpa.val)
-            inorder(rootpa.right)
-            
-        inorder(root)
-        # print(res)
-        res.sort()
-        ii=0
-        def inordercrct(rootka,i):
-            if not rootka:
-                return
-            inordercrct(rootka.left,i)
-            rootka.val=res[0]
-            res.pop(0)
-            inordercrct(rootka.right,i)
-            
-        inordercrct(root,ii)
-        return root
+            dfs(node.left)
+            temp.append(node)
+            dfs(node.right)
+        dfs(root)
+        srt = sorted(n.val for n in temp)
+        for i in range(len(srt)):
+            temp[i].val = srt[i]
         
